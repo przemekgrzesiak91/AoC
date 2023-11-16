dca7e2f3760943a93c4c51410902c6e3296598d9
przemekgrzesiak91 <przemek.grzesiak91@gmail.com>
2023-11-16 09:29:26 +0100

    added part 2 for day 3 (2017)

--- 2017/day3.py
+++ 2017/day3.py
@@ -47,8 +47,60 @@ def count_steps(number):
     return (abs(cords[0])+abs(cords[1]))
 
 
+def check_neighbours(spiral, current_pos):
+    directions = [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)]
+    sum = 0
+    for direction in directions:
+        neighbour = (current_pos[0]+direction[0],current_pos[1]+direction[1])
+        if neighbour in spiral:
+            sum += spiral[neighbour]
+    return sum
 
-#count_steps(int(input))
+def create_spiral(number):
+    spiral = {(0,0):1}
+
+    r = 1
+    current_pos = (0,0)
+    while True:
+        current_pos = (current_pos[0] + 1, current_pos[1])
+        spiral[current_pos] = check_neighbours(spiral,current_pos)
+        #while current_pos!=r or current_pos!=-r:
+        while current_pos[1]< r:
+            current_pos = (current_pos[0],current_pos[1]+1)
+            value=check_neighbours(spiral, current_pos)
+            if value > number:
+                return value
+            else:
+                spiral[current_pos] = value
+
+        while current_pos[0]>-r:
+            current_pos = (current_pos[0]-1, current_pos[1])
+            value = check_neighbours(spiral, current_pos)
+            if value > number:
+                return value
+            else:
+                spiral[current_pos] = value
+
+        while current_pos[1]>-r:
+            current_pos = (current_pos[0], current_pos[1]-1)
+            value = check_neighbours(spiral, current_pos)
+            if value > number:
+                return value
+            else:
+                spiral[current_pos] = value
+        while current_pos[0]<r:
+            current_pos = (current_pos[0]+1, current_pos[1])
+            value = check_neighbours(spiral, current_pos)
+            if value > number:
+                return value
+            else:
+                spiral[current_pos] = value
+        r += 1
+
+
+
 rp1 = count_steps(int(input))
+rp2 = create_spiral(int(input))
+
 
 result(day,year,rp1,rp2)
