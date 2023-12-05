14c92528be376c9d7b0007f2137050a55c45061b
Przemek Grzesiak <przemek.grzesiak91@gmail.com>
2023-12-05 18:45:27 +0100

    Added day 3 part 1 solution

--- 2023/day3.py
+++ 2023/day3.py
@@ -1,5 +1,6 @@
 # Day 3 (2023)
 from colorama import Fore, init
+import numpy as np
 
 # Initialize colorama
 init(autoreset=True)
@@ -10,12 +11,61 @@ year = 2023 #Edit this
 def parse():
     filepath = 'data/day' + str(day) + '.txt'
     with open(filepath, 'r') as f:
-        input = f.readlines()
+        input = f.read()
     return input
 
 def solve_part1(input):
     """Solve part 1."""
-    return 0
+    #print(set(input)) #what with '\n'
+    symbols = {'+', '=', '#', '*', '@', '&', '-', '%', '/', '$'}
+    nums = [str(x) for x in range(0,10)]
+
+    def check_adjacent(value_pos):
+        min_i = value_pos[0][0] - 1
+        if min_i < 0: min_i = 0
+        min_j = value_pos[0][1] - 1
+        if min_j < 0: min_j = 0
+
+        max_i = value_pos[0][0] + 2
+        if max_i > ncol-1: max_i = ncol
+        max_j = value_pos[-1][1] + 2
+        if max_j > ncol-1: max_j = ncol
+        #print(matrix[min_i:max_i,min_j:max_j])
+        adj = matrix[min_i:max_i,min_j:max_j]
+        for symbol in symbols:
+            if symbol in adj:
+                return True
+
+        return False
+    rp1 = 0
+    nrow = input.count('\n')
+    ncol = int((len(input) - nrow)/nrow)
+
+    #print(ncol,nrow)
+    matrix = np.matrix(list(''.join(input.replace('\n',''))))
+    matrix = np.reshape(matrix,(nrow,ncol))
+
+    for i in range(0,nrow):
+        value_pos = []
+        value = ''
+        for j in range(0,ncol):
+            if matrix[i,j] in nums:
+                value_pos.append((i,j))
+                value += matrix[i,j]
+
+            else:
+                if value_pos != []:
+                    #print(value_pos, value)
+                    if check_adjacent(value_pos): rp1 += int(value)
+                    value_pos = []
+                    value = ''
+        if value_pos != []:
+            if check_adjacent(value_pos): rp1 += int(value)
+            value_pos = []
+            value = ''
+
+
+    return rp1
 
 def solve_part2(input):
     """Solve part 2."""
