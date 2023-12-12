64820487f6b811d532402bcd1ba97f0ee2373bbb
Przemek Grzesiak <przemek.grzesiak91@gmail.com>
2023-12-12 17:15:59 +0100

    Added part 2 for day 8 (2023)

--- 2023/day8.py
+++ 2023/day8.py
@@ -1,5 +1,6 @@
 # Day 8 (2023)
 from colorama import Fore, init
+import math
 
 # Initialize colorama
 init(autoreset=True)
@@ -41,7 +42,38 @@ def solve_part1(input):
 
 def solve_part2(input):
     """Solve part 2."""
-    return 0
+    nav = input[0]
+    nodes = {}
+    len_nav = len(nav)
+    current = []
+    n_steps_Z = [0,0,0,0,0,0]
+    steps = 0
+    i= 0
+    for row in input[2:]:
+        key, value = row.strip().split(' = ')
+        nodes[key] = value[1:-1].split(', ')
+
+    for key in nodes.keys():
+        if key[2] == 'A': current.append(key)
+
+    last_letters = ''.join([x[2] for x in current])
+
+    while 0 in n_steps_Z:
+        for j in range(0, len(current)):
+            if nav[i] == 'L':
+                current[j] = nodes[current[j]][0]
+            elif nav[i] == 'R':
+                current[j] = nodes[current[j]][1]
+
+            if current[j][2] == 'Z' and n_steps_Z[j] == 0:
+                n_steps_Z[j] = steps+1
+        steps += 1
+        i += 1
+
+        if i >= len_nav - 1: i = 0
+
+    return math.lcm(*n_steps_Z)
+
 
 def result(day,year):
     input = parse()
